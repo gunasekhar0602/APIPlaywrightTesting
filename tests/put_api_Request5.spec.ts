7b08f15815668b594891e47eae501efc0254b6d3
guna <gunabandla2000@gmail.com>
2026-04-26 12:09:56 +0530

    Update

--- tests/put_api_Request5.spec.ts
+++ tests/put_api_Request5.spec.ts
@@ -29,7 +29,33 @@ test('update Booking(PUT)',async({request})=>
 
     // 2) Update booking (PUT)  // required token
     // token creation
-    const tokenrequestbody=readJson("testdata/token_requestbody.json")
+
+    const tokenrequestbody=readJson("testdata/token_requestbody.json");
+    const tokenresponse=await request.post("https://restful-booker.heroapp.com/auth",
+                                                                    {data:tokenrequestbody});
+    expect(tokenresponse.ok()).toBeTruthy();
+
+    const tokenresponsebody=await tokenresponse.json();
+    const token=tokenresponsebody.token();
+    console.log("Token", token)
+
+
+    // 3) Sending update (Put)
+
+    // For update request along with the request we need to token through headers
+    const updaterequestbody =readJson("testdata/putrequestbody.json");
+    const updateresponse=await request.put(`https://restful-booker.heroapp.com/booking/ ${bookingid}`,
+                    {
+                            headers:{"cookie":`token=${token}`},
+                                        data:updaterequestbody
+        
+                    })
+    expect(updateresponse.ok()).toBeTruthy();
+    expect(updateresponse.status()).toBe(200);
+
+    const updateresponsebody=await updateresponse.json();
+    console.log(updateresponsebody);
+    console.log("Booking details updated successfully.....");
 
 
 })
